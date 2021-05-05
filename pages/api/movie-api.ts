9ffc3263614b26068e1db039a51e559e27f5feb8
mah51 <mah51@kent.ac.uk>
2021-05-05 06:23:30 +0100

    hmm

--- pages/api/movie-api.ts
+++ pages/api/movie-api.ts
@@ -3,14 +3,19 @@ import { useAPIAuth } from '../../utils/useAPIAuth';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const discUser = await useAPIAuth(req, res, process.env.JWT_CODE);
-  if (!discUser) {
-    return res.status(401);
-  }
-  const { search } = req.query;
+  try {
+    if (!discUser) {
+      return res.status(401);
+    }
+    const { search } = req.query;
 
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`,
-  );
-  const data = await response.json();
-  return res.status(response.status).send(data);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`,
+    );
+    const data = await response.json();
+    return res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
 };
