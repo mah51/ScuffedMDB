6e2cb8b3336d1c3137351a518aafb13fa7b6b5bf
mah51 <mikiemike.hall@gmail.com>
2021-05-23 21:05:58 +0100

    adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import PlausibleProvider from 'next-plausible';
 
 const theme = extendTheme({
   colors: {
@@ -15,11 +16,19 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <PlausibleProvider
+      domain="michael-hall.me"
+      selfHosted
+      trackOutboundLinks
+      enabled={process.env.NODE_ENV === 'production'}
+      customDomain={'https://stats.michael-hall.me'}
+    >
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </PlausibleProvider>
   );
 }
 
