a27a498738855bebdc9ad3ad03d0f2466fed72f8
mah51 <mah51@kent.ac.uk>
2021-05-31 03:54:16 +0100

    Fixing order of movies

--- pages/api/movie.ts
+++ pages/api/movie.ts
@@ -1,102 +1,105 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { verify } from 'jsonwebtoken';
-import { parse } from 'cookie';
-import axios from 'axios';
-import { DiscordUser } from '../../types/generalTypes';
-import Movie, { MovieType } from '../../models/movie';
-import User from '../../models/user';
-import dbConnect from '../../utils/dbConnect';
-import { MovieEndpointBodyType } from '../../types/APITypes';
-import { useAPIAuth } from '../../utils/useAPIAuth';
+import { NextApiRequest, NextApiResponse } from "next";
+import { verify } from "jsonwebtoken";
+import { parse } from "cookie";
+import axios from "axios";
+import { DiscordUser } from "../../types/generalTypes";
+import Movie, { MovieType } from "../../models/movie";
+import User from "../../models/user";
+import dbConnect from "../../utils/dbConnect";
+import { MovieEndpointBodyType } from "../../types/APITypes";
+import { useAPIAuth } from "../../utils/useAPIAuth";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await dbConnect();
-  if (req.method === `POST`) {
-    if (!req.headers.cookie) {
-      return null;
+    await dbConnect();
+    if (req.method === `POST`) {
+        if (!req.headers.cookie) {
+            return null;
+        }
+
+        const { token } = parse(req.headers.cookie);
+        if (!token) {
+            return null;
+        }
+        const { id: movieID }: MovieEndpointBodyType = JSON.parse(req.body);
+        try {
+            const { iat, exp, ...user } = verify(
+                token,
+                process.env.JWT_CODE
+            ) as DiscordUser & { iat: number; exp: number };
+
+            const discUser = await User.findOne({ id: user.id });
+            if (!discUser) {
+                return res.status(401);
+            }
+
+            const { data: movieData, status: movieStatus } = await axios.get(
+                `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`
+            );
+
+            if (movieStatus !== 200 || movieData.status_code) {
+                return res.status(movieStatus);
+            }
+
+            const existingMovie = await Movie.findOne({ movieID });
+            if (existingMovie) {
+                return res
+                    .status(400)
+                    .send({ message: `Movie already exists!` });
+            }
+
+            const genres = movieData?.genres?.map((genre) => {
+                return genre.name;
+            });
+
+            const newMovie: MovieType = new Movie({
+                name: movieData.original_title,
+                movieID,
+                genres: [...genres],
+                image: `https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`,
+                description: movieData.overview,
+                tagLine: movieData.tagline,
+                rating: 0,
+                numReviews: 0,
+                reviews: [],
+            });
+            await newMovie.save();
+            return res.status(200).send({ data: newMovie, type: `addition` });
+        } catch (err) {
+            console.error(err);
+            return res.status(500);
+        }
+    } else if (req.method === `GET`) {
+        try {
+            const movies = await Movie.find({}).populate(
+                `reviews.user`,
+                `avatar username id discriminator`
+            );
+
+            return res.status(200).send({ data: movies });
+        } catch (err) {
+            console.error(err);
+            return res.status(500);
+        }
+    } else if (req.method === `DELETE`) {
+        const { id } = JSON.parse(req.body);
+        const discUser = await useAPIAuth(req, res);
+        if (!discUser || !discUser.isAdmin) {
+            return res
+                .status(401)
+                .json({ message: `You are unauthorized to use that :(` });
+        }
+
+        const movie = await Movie.findOne({ _id: id });
+        if (!movie) {
+            return res.status(404);
+        }
+        const deletedMovie = await Movie.deleteOne({ _id: id });
+        if (deletedMovie.ok === 1) {
+            return res.status(200).json(movie);
+        }
+
+        return res.status(500);
+    } else {
+        return res.status(405).json({ message: `method not allowed` });
     }
-
-    const { token } = parse(req.headers.cookie);
-    if (!token) {
-      return null;
-    }
-    const { id: movieID }: MovieEndpointBodyType = JSON.parse(req.body);
-    try {
-      const { iat, exp, ...user } = verify(
-        token,
-        process.env.JWT_CODE,
-      ) as DiscordUser & { iat: number; exp: number };
-
-      const discUser = await User.findOne({ id: user.id });
-      if (!discUser) {
-        return res.status(401);
-      }
-
-      const { data: movieData, status: movieStatus } = await axios.get(
-        `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`,
-      );
-
-      if (movieStatus !== 200 || movieData.status_code) {
-        return res.status(movieStatus);
-      }
-
-      const existingMovie = await Movie.findOne({ movieID });
-      if (existingMovie) {
-        return res.status(400).send({ message: `Movie already exists!` });
-      }
-
-      const genres = movieData?.genres?.map((genre) => {
-        return genre.name;
-      });
-
-      const newMovie: MovieType = new Movie({
-        name: movieData.original_title,
-        movieID,
-        genres: [...genres],
-        image: `https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`,
-        description: movieData.overview,
-        tagLine: movieData.tagline,
-        rating: 0,
-        numReviews: 0,
-        reviews: [],
-      });
-      await newMovie.save();
-      return res.status(200).send({ data: newMovie, type: `addition` });
-    } catch (err) {
-      console.error(err);
-      return res.status(500);
-    }
-  } else if (req.method === `GET`) {
-    try {
-      const movies = await Movie.find({}).populate(
-        `reviews.user`,
-        `avatar username id discriminator`,
-      );
-      return res.status(200).send({ data: movies });
-    } catch (err) {
-      console.error(err);
-      return res.status(500);
-    }
-  } else if (req.method === `DELETE`) {
-    const { id } = JSON.parse(req.body);
-    const discUser = await useAPIAuth(req, res);
-    if (!discUser || !discUser.isAdmin) {
-      return res
-        .status(401)
-        .json({ message: `You are unauthorized to use that :(` });
-    }
-
-    const movie = await Movie.findOne({ _id: id });
-    if (!movie) {
-      return res.status(404);
-    }
-    const deletedMovie = await Movie.deleteOne({ _id: id });
-    if (deletedMovie.ok === 1) {
-      return res.status(200).json(movie);
-    }
-
-    return res.status(500);
-  } else {
-    return res.status(405).json({ message: `method not allowed` });
-  }
 };
--- utils/queries.ts
+++ utils/queries.ts
@@ -1,15 +1,23 @@
 export const getMovies = async () => {
     const res: Response = await fetch(
-        `${process.env.NEXT_PUBLIC_APP_URI}/api/movie`,
+        `${process.env.NEXT_PUBLIC_APP_URI}/api/movie`
     );
     // eslint-disable-next-line no-return-await
-    const movies = await res.json();
-    return movies.data.sort((a, b) => a.createdAt - b.createdAt).reverse();
+    const unsortedMovies = await res.json();
+
+    const movies = unsortedMovies.data
+        .sort(
+            (a, b) =>
+                new Date(a.createdAt).getTime() -
+                new Date(b.createdAt).getTime()
+        )
+        .reverse();
+    return movies;
 };
 
 export const getUsers = async () => {
     const res: Response = await fetch(
-        `${process.env.NEXT_PUBLIC_APP_URI}/api/users`,
+        `${process.env.NEXT_PUBLIC_APP_URI}/api/users`
     );
     const data = await res.json();
     return data.users;
