dde9a0fc0629f8538d829b0b665b31b8aeb4b4f6
mah51 <mah51@kent.ac.uk>
2021-07-10 05:51:26 +0100

    user schema isnt defined before db call

--- utils/queries.ts
+++ utils/queries.ts
@@ -1,6 +1,11 @@
 import { MovieType } from '../models/movie';
+import user from '../models/user';
+import dbConnect from './dbConnect';
 
 export const getMovies = async (): Promise<MovieType[]> => {
+  await dbConnect();
+  //Scuffed, but makes sure that Schema gets registered before next-auth tries to access it.
+  await user.findOne({ id: '1' });
   const res: Response = await fetch(
     `${process.env.NEXT_PUBLIC_APP_URI}/api/movie`
   );
