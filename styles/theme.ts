9803ee0f5db36b9ae7c6734aa1bccac581c7db3e
mah51 <mikiemike.hall@gmail.com>
2021-08-17 23:12:45 +0100

    add more breakpoints for larger screens

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,25 @@
 // 1. import `extendTheme` function
-import { ColorMode, extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { Button, Switch } from './component_styles';
 
 // 2. Add your color mode config
+
 interface CustomTheme extends ThemeConfig {
-  initialColorMode: ColorMode;
   useSystemColorMode: boolean;
 }
 
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em', //1280px
+  '2xl': '96em',
+  '3xl': '120em', //1920px
+  '4xl': '160em', // 2560px
+});
+
 const config: CustomTheme = {
-  initialColorMode: `dark`,
   useSystemColorMode: true,
 };
 
@@ -21,6 +31,7 @@ const components = {
 // 3. extend the theme
 const theme = extendTheme({
   config,
+  breakpoints,
   components,
   colors: {
     brand: {
